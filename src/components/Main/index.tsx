bd96e86c8c6d2c3df4945e7277beb23432528e26
sunchaehoon <sunchaehoon@naver.com>
2022-12-27 16:18:13 +0900

    Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,23 +3,77 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import * as S from "./style";
 import Navbar from "../Navbar";
+import { useRouter } from "next/router";
 
 const Main: NextPage = () => {
   const [showNav, setShowNav] = useState<boolean>(false);
+  const [clickMypg, setClickMypg] = useState<boolean>(false);
+  const [clickSchedule, setClickSchedule] = useState<boolean>(false);
+  const [clickFeed, setClickFeed] = useState<boolean>(false);
+  const [clickLuck, setClickLuck] = useState<boolean>(false);
+  const [showArrow, setShowArrow] = useState("none");
+  const router = useRouter();
+  const ro = router.pathname;
 
   function NavClick() {
     setShowNav((prev) => !prev);
   }
 
+  function CreateSnow() {
+    const el = document.createElement("div");
+    el.classList.add('snow');
+    el.style.marginLeft = Math.floor(Math.random() * window.innerWidth) + 'px';
+    document.body.appendChild(el);
+  }
+
   useEffect(() => {
-    console.log(showNav);
-  }, [showNav]);
+    for(let i=0; i<50; i++) {
+      CreateSnow();
+    }
+    if(clickMypg === true || clickSchedule === true || clickFeed === true || clickLuck === true) {
+      setShowArrow("block");
+    }
+  }, [clickMypg, clickSchedule, clickFeed, clickLuck]);
+  
 
   return (
     <>
       <S.Background>
-      <S.NavLine onClick={NavClick} />
-        <Navbar showNav={showNav} />
+        <S.NavLine onClick={NavClick} />
+        <Navbar
+          showNav={showNav}
+          setClickMypg={setClickMypg}
+          setClickSchedule={setClickSchedule}
+          setClickFeed={setClickFeed}
+          setClickLuck={setClickLuck}
+        />
+        <S.Capsule>
+          <S.CapsuleTxt>
+            {clickMypg === true
+              ? "마이페이지"
+              : clickSchedule === true
+              ? "신년 계획 쓰러가기"
+              : clickFeed === true
+              ? "신년 피드"
+              : clickLuck === true
+              ? "신년 운세 보러가기"
+              : ""}
+          </S.CapsuleTxt>
+          <S.Button display={showArrow}>
+            <Link href={
+                clickMypg === true
+                ? "/Mypg"
+                : clickSchedule === true
+                ? "/Schedule"
+                : clickFeed === true
+                ? "/Feed"
+                : clickLuck === true
+                ? "/Luck"
+                : ""
+              }
+            ><S.Arrow  /></Link>
+          </S.Button>
+        </S.Capsule>
       </S.Background>
     </>
   );
--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -1,25 +1,21 @@
 import styled from "@emotion/styled";
 
-export const Wrapper = styled.div`
-    width: 100%;
-    height: 100%;
-`
-
 export const Background = styled.div`
-    background-image: url("/img/Background2.png");
-    background-size: cover;
+    background-image: url("/img/Background3.png");
     position: fixed;
+    background-size: 100% 100%;
     top: 0;
     left: 0;
     width: 100%;
-    height: 100%;
+    height: 100vh;
     background-repeat: no-repeat;
+    z-index: 0;
 `
 
 export const NavLine = styled.div`
     margin: 30px 30px 0 0;
     background-image: url("/img/Bar.png");
-    position: absolute;
+    position: absolute; 
     right: 0;
     top: 0;
     width: 45px;
@@ -27,7 +23,53 @@ export const NavLine = styled.div`
     background-size: 100%;
     background-repeat: no-repeat;
     cursor: pointer;
-    z-index: 2;
+    z-index: 3;
+`
+
+export const Capsule = styled.div`
+    margin: 0 auto;
+    margin-top: 40vh;
+    width: 30%;
+    display: block;
+`
+
+export const CapsuleTxt = styled.div`
+    color: #ffffff;
+    font-size: 50px;
+    text-align: center;
+    @media screen and (max-width: 1300px) {
+        font-size: 40px;
+    }
+    @media screen and (max-width: 1100px) {
+        font-size: 32px;
+    }
+    @media screen and (max-width: 900px) {
+        font-size: 24px;
+    }
+`
+
+export const Button = styled.div<{display: string}>`
+    position: relative;
+    margin: 0 auto;
+    margin-top: 40px;
+    background-color: #ffffff;
+    width: 15rem;
+    height: 4.8rem;
+    border-radius: 15px;
+    display: ${(props) => props.display};
+`
+
+export const Arrow = styled.div`
+    height: 100%;
+    margin: 0 27%;
+    background-image: url("/img/Arrow.png");
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    cursor: pointer;
+    :hover {
+        transform: scale(1.15);
+        transition-duration: 0.25s;
+    }
 `
 
 
