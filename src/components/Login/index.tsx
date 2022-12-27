0154ff02343d4c60fe4eca37bbcfcaf237e546ab
sunchaehoon <sunchaehoon@naver.com>
2022-12-27 16:17:54 +0900

    Login

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,99 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import * as S from "./style";
+import Navbar from "../Navbar";
+import { useRouter } from "next/router";
+
+const Login: NextPage = () => {
+  const [idValue, setIdValue] = useState("");
+  const [pwValue, setPwValue] = useState("");
+  const [noId, setNoId] = useState(false);
+  const [noPw, setNoPw] = useState<boolean>(false);
+  const router = useRouter();
+  const ro = router.pathname;
+  const currentUrl = router.asPath;
+
+  function CreateSnow() {
+    const el = document.createElement("div");
+    el.classList.add("snow");
+    el.style.marginLeft = Math.floor(Math.random() * window.innerWidth) + "px";
+    document.body.appendChild(el);
+  }
+
+  useEffect(() => {
+    console.log(ro);
+    console.log(currentUrl);
+    for (let i = 0; i < 100; i++) {
+      CreateSnow();
+    }
+    if (idValue !== "") {
+      setNoId(false);
+    }
+    if (pwValue !== "") {
+      setNoPw(false);
+    }
+  }, [idValue, pwValue]);
+
+  function ChangeIdInput(e: any) {
+    setIdValue(e.target.value);
+  }
+
+  function ChangePwInput(e: any) {
+    setPwValue(e.target.value);
+  }
+
+  function SubmitLogin() {
+    if (idValue === "") {
+      setNoId(true);
+    } else {
+      setNoId(false);
+    }
+    if (pwValue === "") {
+      setNoPw(true);
+    } else {
+      setNoPw(false);
+    }
+  }
+
+  return (
+    <>
+      <S.Background>
+        <S.LoginBackground>
+          <Link href="/"><S.BackArrow /></Link>
+          <S.LoginWrap>
+            <S.LoginTxt>
+              <S.LoginLO>LO</S.LoginLO>
+              <S.LoginGIN>GIN</S.LoginGIN>
+            </S.LoginTxt>
+            <S.InputDiv>
+              <S.InputIdTxt>아이디</S.InputIdTxt>
+              <S.InputId onChange={ChangeIdInput} />
+              <S.NoInput visibility={noId === true ? "visible" : "hidden"}>
+                아이디를 입력해주세요
+              </S.NoInput>
+            </S.InputDiv>
+            <S.InputDiv>
+              <S.InputPwTxt>비밀번호</S.InputPwTxt>
+              <S.InputPw onChange={ChangePwInput} type="password" />
+              <S.NoInput visibility={noPw === true ? "visible" : "hidden"}>
+                비밀번호를 입력해주세요
+              </S.NoInput>
+            </S.InputDiv>
+            <S.LoginCheck>
+              <S.LoginBtn onClick={SubmitLogin}>로그인</S.LoginBtn>
+            </S.LoginCheck>
+            <S.NoAccountWrap>
+              <S.NoAccountTxt>계정이 없으신가요?</S.NoAccountTxt>
+              <S.Register>
+                <Link href="/Signup">회원가입</Link>
+              </S.Register>
+            </S.NoAccountWrap>
+          </S.LoginWrap>
+        </S.LoginBackground>
+      </S.Background>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/Login/style.ts
@@ -0,0 +1,152 @@
+import styled from "@emotion/styled";
+
+export const Background = styled.div`
+    background-image: url("/img/Background3.png");
+    position: fixed;
+    background-size: 100% 100%;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-repeat: no-repeat;
+`
+
+export const LoginBackground = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 2;
+`
+
+export const LoginWrap = styled.div`
+    width: 30%;
+    height: 540px;
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 30px;
+    padding: 55px 50px;
+    display: block;
+    text-align: center;
+    z-index: 2;
+`
+
+export const LoginTxt = styled.div`
+   display: flex;
+   justify-content: center;
+`
+
+export const LoginLO = styled.div`
+    font-size: 4rem;
+    color: #FF5A5A;
+    padding-bottom: 7px;
+    border-bottom: 1px solid black;
+`
+
+export const LoginGIN = styled.div`
+    font-size: 4rem;
+    color: #26971C;
+    padding-bottom: 7px;
+    border-bottom: 1px solid black;
+`
+
+export const InputDiv = styled.div`
+    margin-top: 35px;
+    display: block;
+`
+
+export const InputIdTxt = styled.div`
+    text-align: left;
+    color: #FF5A5A;
+    font-size: 1.3rem;
+    padding-left: 20px;
+`
+
+export const InputPwTxt = styled(InputIdTxt)`
+    color: #26971C;
+`
+
+export const InputId = styled.input`
+    margin-top: 5px;
+    border-radius: 40px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    height: 35px;
+    padding-left: 12px;
+    font-size: 18px;
+`
+
+export const InputPw = styled(InputId)`
+    font-family: 'Malgun gothic', dotum, sans-serif !important;
+`
+
+export const LoginCheck = styled.div`
+    margin: 0 auto;
+    margin-top: 40px;
+    width: 50%;
+    height: 50px;
+    border-radius: 50px;
+    :hover {
+        background-color: #FBFCF3;
+        transition-duration: 0.28s;
+    }
+`
+
+export const LoginBtn = styled.div`
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.6rem;
+    height: 100%;
+    cursor: pointer;
+`
+
+export const NoAccountWrap = styled.div`
+    margin-top: 75px;
+    display: flex;
+    justify-content: center;
+`
+
+export const NoAccountTxt = styled.div`
+    color: #525252;
+    padding-right: 7px;
+    font-size: 1.1rem;
+`
+
+export const Register = styled.div`
+    color: #FF5A5A;
+    font-size: 1.1rem;
+    cursor: pointer;
+`
+
+export const NoInput = styled.div<{visibility: string}>`
+    color: #FF5A5A;
+    text-align: left;
+    padding-top: 3px;
+    padding-left: 10px;
+    font-size: 0.8rem;
+    visibility: ${(props) => props.visibility};
+`
+
+export const BackArrow = styled.div`
+    background-image: url("/img/BackArrow.png");
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: 30px 0 0 20px;
+    width: 50px;
+    height: 40px;
+    background-size: 100%;
+    cursor: pointer;
+    :hover {
+        transform: scale(1.2);
+        transition-duration: 0.3s;
+    }
+`
+
+
+
+
